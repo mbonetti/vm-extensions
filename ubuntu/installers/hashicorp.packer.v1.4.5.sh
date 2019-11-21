e2d234d52d55441e5c937eb6809b601b3089adc3
Michael Bonetti <michael.bonetti@incyclesoftware.com>
2019-11-21 17:34:47 -0500

    use curl instead of wget

--- ubuntu/installers/hashicorp.packer.v1.4.5.sh
+++ ubuntu/installers/hashicorp.packer.v1.4.5.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export VER="1.4.5"
-nwget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
+curl -LO https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
+#gunzip -S .zip packer_${VER}_linux_amd64.zip
+#nwget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
 unzip packer_${VER}_linux_amd64.zip
 sudo mv packer /usr/local/bin
