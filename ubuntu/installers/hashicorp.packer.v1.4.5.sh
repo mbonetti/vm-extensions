bc6f858697eed16de6dc54867dbd75dcc163c506
Michael Bonetti <michael.bonetti@incyclesoftware.com>
2019-11-22 10:00:10 -0500

    add script to get agent download url

--- ubuntu/installers/hashicorp.packer.v1.4.5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export VER="1.4.5"
-curl -LO https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
-#gunzip -S .zip packer_${VER}_linux_amd64.zip
-#nwget https://releases.hashicorp.com/packer/${VER}/packer_${VER}_linux_amd64.zip
-unzip packer_${VER}_linux_amd64.zip
-sudo mv packer /usr/local/bin
